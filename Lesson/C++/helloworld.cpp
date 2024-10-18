99a244a7362c12f55e761a8c416c321427822a03
LogicMastery <silicon.logic.tech@gmail.com>
2024-10-19 00:02:29 +0800

    first c++ program

--- Lesson/C++/helloworld.cpp
+++ Lesson/C++/helloworld.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main(){
-cout << "Hello World" << endl;
+string Name = "Dale";
+int Age = 27;
 
+cout << "What is your name?";
+cin >> Name; 
+cout << "Next question is, how old are you?";
+cin >> Age;
+cout << "Your name is " << Name << " your " << Age << " years old.";
 return 0;
 }
 
